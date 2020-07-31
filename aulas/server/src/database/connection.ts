e820278c945278e6243c17484481c383f3b8875d
Samuca-sys <samuel.mferreira@hotmail.com>
2020-07-31 08:04:53 -0300

    chore: add controllers, cors

--- aulas/server/src/database/connection.ts
+++ aulas/server/src/database/connection.ts
@@ -1,7 +1,7 @@
 import knex, { ConnectionConfig } from 'knex'; 
 import path from 'path';
 
-const conection = knex({
+const connection = knex({
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'), 
