63274a7e7429590f52411e8f6695dafae3446b51
Samuca-sys <samuel.mferreira@hotmail.com>
2020-07-31 19:53:54 -0300

    chore: explain React concepts

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
+  const [counter, setCounter]  = useState(0);
+
+  function handledButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Header title={`Contador: ${counter}`} />
+      
+      <h1>{counter}</h1>
+      <button type="button" onClick={handledButtonClick}>Aumentar</button>
+    </div>  
   );
 }
 
